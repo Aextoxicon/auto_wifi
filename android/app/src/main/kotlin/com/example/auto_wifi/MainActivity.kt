9c0b77123978202812ba3ab9a902f2117d3d2673
Lwh2008 <lwh20082021@outlook.com>
2025-10-17 01:59:56 +0800

    1

--- android/app/src/main/kotlin/com/example/auto_wifi/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/auto_wifi/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.example.auto_wifi
+import android.os.Build
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+
+import android.os.Build
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
