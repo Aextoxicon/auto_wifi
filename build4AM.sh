a07237b58dcbdc0e4bd2b28c197cce80d772a6ae
lwhs <lwh20082021@outlook.com>
2026-01-14 23:00:08 +0800

    .github to .tmp

--- build4AM.sh
+++ build4AM.sh
@@ -2,3 +2,4 @@ flutter pub run flutter_launcher_icons && flutter build apk --release && flutter
 cp build/app/outputs/flutter-apk/app-release.apk auto_wifi_android.apk
 zip -r -X auto_wifi_mac.zip build/macos/Build/Products/Release/auto_wifi.app
 curl --http1.1 -X POST -H "Authorization: Bearer $API_TOKEN" -F "file=@auto_wifi_mac.zip" https://update.aextoxicon.site/upload
+curl --http1.1 -X POST -H "Authorization: Bearer $API_TOKEN" -F "file=@auto_wifi_android.apk" https://update.aextoxicon.site/upload
